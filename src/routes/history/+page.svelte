fa8789dc2bf0019a6d5132e5d3439b60b3274f26
Memet Zx <zxce3@gnuweeb.org>
2023-12-11 12:17:05 +0700

    add "History" this where we tell the history
    it's not completed yet, but it's oke
    
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,69 @@
+<svelte:head>
+    <title>History - Desa Seberida</title>
+</svelte:head>
+
+<section class="max-w-3xl mx-auto p-4">
+    <h2 class="text-2xl font-bold mb-4">
+        Discover the Rich History of Seberida!
+    </h2>
+    <p>
+        Welcome to the captivating history of Seberida, a village nestled in the
+        heart of the Batang Gansal sub-district within the enchanting Indragiri
+        Hulu Regency. Let's embark on a journey through time to explore the
+        vibrant tapestry that makes Seberida a truly unique and fascinating
+        community.
+    </p>
+
+    <h3 class="text-xl font-bold mb-2">Inauguration and Diverse Roots</h3>
+    <p>
+        The story of Seberida begins around 1918 AD when Muhammad ISA, the first
+        Wali Negeri, assumed leadership. Since its inception, Seberida has been
+        a melting pot of diverse ethnicities, including Malays, Javanese,
+        Sundanese, Batak, and various other migrant communities. This diversity
+        has become a strength, portraying unity in diversity and fostering a
+        harmonious balance among the coexisting communities.
+    </p>
+
+    <h3 class="text-xl font-bold mb-2">
+        Leadership Transitions and Regional Transformations
+    </h3>
+    <p>
+        Over the years, the village has witnessed ten changes in leadership.
+        These leaders not only represented their respective eras but also played
+        a pivotal role in the transformation of the region. Seberida evolved
+        from a vast area encompassing Seberida sub-district to administrative
+        entities such as Seberida village and Kota Baru Seberida.
+    </p>
+
+    <h3 class="text-xl font-bold mb-2">The Origin of "Seberida"</h3>
+    <p>
+        The name "Seberida" finds its origin in the intriguing tale of the
+        abundant "belida" fish in the Seberida River. Over time, the name
+        underwent linguistic transformations, becoming the distinctive identity
+        of the village—symbolizing abundance and sustainable living.
+    </p>
+
+    <h3 class="text-xl font-bold mb-2">
+        Leadership Dedication and Community Building
+    </h3>
+    <p>
+        From Muhammad ISA to the current village head, Ria Saprina, each leader
+        dedicated themselves to making Seberida a better place for its
+        residents. The village looks towards a future full of hope, built upon
+        its rich history and diverse community.
+    </p>
+
+    <h3 class="text-xl font-bold mb-2">Towards a Promising Future</h3>
+    <p>
+        With a rich history and a diverse community, Seberida strides
+        confidently into a future filled with promise. The historical legacy
+        serves as a robust foundation, guiding the community in building a
+        bright future for generations to come.
+    </p>
+
+    <p>
+        Seberida Village, a living story that continues to evolve, dedicates
+        itself to remaining an integral part of heritage and sustainability.
+        Enjoy the journey through the long and fascinating history of Seberida!
+    </p>
+</section>
