6f729157e5c1151850eff377ef9faeafed136930
mdswa <mdswa@users.noreply.github.com>
2025-02-01 20:52:20 +0000

    Deploy: Deploy to GitHub Pages
    Deployed by GitHub Actions at 2025-02-01 20:52:20 UTC
    Build from commit: f65693b702a9f8c08119aadb8ce4063fb610eb9d

---
+++ _app/immutable/nodes/5.e7354097.js
@@ -0,0 +1,25 @@
+import{s as h,n}from"../chunks/scheduler.e108d1fd.js";import{S as d,i as u,s as c,g as m,z as g,f as s,c as b,h as p,x as f,k as v,a as r}from"../chunks/index.bad58c3a.js";function y(l){let i,e,o=`<h2 class="text-2xl font-bold mb-4">Discover the Rich History of Seberida!</h2> <p>Welcome to the captivating history of Seberida, a village nestled in the
+        heart of the Batang Gansal sub-district within the enchanting Indragiri
+        Hulu Regency. Let&#39;s embark on a journey through time to explore the
+        vibrant tapestry that makes Seberida a truly unique and fascinating
+        community.</p> <h3 class="text-xl font-bold mb-2">Inauguration and Diverse Roots</h3> <p>The story of Seberida begins around 1918 AD when Muhammad ISA, the first
+        Wali Negeri, assumed leadership. Since its inception, Seberida has been
+        a melting pot of diverse ethnicities, including Malays, Javanese,
+        Sundanese, Batak, and various other migrant communities. This diversity
+        has become a strength, portraying unity in diversity and fostering a
+        harmonious balance among the coexisting communities.</p> <h3 class="text-xl font-bold mb-2">Leadership Transitions and Regional Transformations</h3> <p>Over the years, the village has witnessed ten changes in leadership.
+        These leaders not only represented their respective eras but also played
+        a pivotal role in the transformation of the region. Seberida evolved
+        from a vast area encompassing Seberida sub-district to administrative
+        entities such as Seberida village and Kota Baru Seberida.</p> <h3 class="text-xl font-bold mb-2">The Origin of &quot;Seberida&quot;</h3> <p>The name &quot;Seberida&quot; finds its origin in the intriguing tale of the
+        abundant &quot;belida&quot; fish in the Seberida River. Over time, the name
+        underwent linguistic transformations, becoming the distinctive identity
+        of the village—symbolizing abundance and sustainable living.</p> <h3 class="text-xl font-bold mb-2">Leadership Dedication and Community Building</h3> <p>From Muhammad ISA to the current village head, Ria Saprina, each leader
+        dedicated themselves to making Seberida a better place for its
+        residents. The village looks towards a future full of hope, built upon
+        its rich history and diverse community.</p> <h3 class="text-xl font-bold mb-2">Towards a Promising Future</h3> <p>With a rich history and a diverse community, Seberida strides
+        confidently into a future filled with promise. The historical legacy
+        serves as a robust foundation, guiding the community in building a
+        bright future for generations to come.</p> <p>Seberida Village, a living story that continues to evolve, dedicates
+        itself to remaining an integral part of heritage and sustainability.
+        Enjoy the journey through the long and fascinating history of Seberida!</p>`;return{c(){i=c(),e=m("section"),e.innerHTML=o,this.h()},l(t){g("svelte-piphy4",document.head).forEach(s),i=b(t),e=p(t,"SECTION",{class:!0,"data-svelte-h":!0}),f(e)!=="svelte-1k38zhw"&&(e.innerHTML=o),this.h()},h(){document.title="History - Desa Seberida",v(e,"class","max-w-3xl mx-auto p-4")},m(t,a){r(t,i,a),r(t,e,a)},p:n,i:n,o:n,d(t){t&&(s(i),s(e))}}}class T extends d{constructor(i){super(),u(this,i,null,y,h,{})}}export{T as component};
