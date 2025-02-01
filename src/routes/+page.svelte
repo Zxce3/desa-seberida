e95c145a79004844fed0673001f36859a165c2ec
Mandakara sadewa <joeahmad081312@gmail.com>
2025-02-02 03:15:36 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from "svelte";
 
-  const releaseDate = new Date("2024-02-02T00:00:00.000Z");
+  const releaseDate = new Date("2025-02-02T00:00:00.000Z");
 
   let daysRemaining = 0;
   let hoursRemaining = 0;
